804efcb736a3ae6a13132c42e748c4a51337cfa1
peta jeza <jeza@anus.peta.edu>
2024-10-30 11:08:48 +0100

    UKOgL

---
+++ Principy pocitacu/sELFaSSESSNEBT 3.py
@@ -0,0 +1,13 @@
+Napište výsledné hodnoty následujících posunů – výsledek zapište ve stejné soustavě (dvojková nebo
+šestnáctková) v jaké je zapsán levý operand operace posunu. Dodržte uvedenou přesnost operace:
+8-bitový posun: 00101110 SHL 5 -> 11000000
+8-bitový posun: 00101110 SHL 16 -> 00000000
+8-bitový posun: 0x1C SHL 4 -> 0xC0
+32-bitový posun: 0x00017000 SHL 3 -> 0xB8000
+32-bitový posun: 0x84ACFF09 SHL 8 -> ACFF0900
+32-bitový posun: 0x84ACFF09 SHL 12 -> CFF09000
+8-bitový posun: 10101110 SHR 5 -> 101
+8-bitový posun: 10101110 SHR 7 -> 1
+8-bitový posun: 00101111 SHR 7 -> 0
+32-bitový posun: 0x84ACFF09 SHR 8 -> 84ACFF
+32-bitový posun: 0x84ACFF09 SHR 12 -> 84ACF
