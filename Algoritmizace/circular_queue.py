1c944e8a1f3448d623931f250467334f133f8475
peta jeza <jeza@anus.peta.edu>
2024-11-20 16:52:53 +0100

    zea

--- Algoritmizace/circular_queue.py
+++ Algoritmizace/circular_queue.py
@@ -34,9 +34,45 @@ class CircularQueue():
     def enqueue(self, n: int):
         if self.checkIfFull():
             self.resize()
-        self.array[self.tail + 1] = n
+        ## DORES PRIDANI PRVKU JINAK VSE V POHO 
+        if self.tail == len(self.array) - 1:
+            self.array[0] = n
+            self.tail = 0
+        else:
+            self.array[self.tail + 1] = n
+            self.tail += 1
     def dequeue(self):
         temp = self.array[self.head]
         self.array[self.head] = None
         return temp
-     
+    def count(self) -> int:
+        none_count = 0
+        for i in self.array:
+            if i == None:
+                none_count += 1
+        return len(self.array) - none_count
+    def avg(self):
+        pocet = 0
+        soucet = 0
+        for i in self.array:
+            if i == None:
+                pass
+            else: 
+                pocet += 1
+                soucet += i
+        return soucet / pocet
+    
+q = CircularQueue(5)
+q.enqueue(6)
+q.enqueue(6)
+q.enqueue(9)
+q.enqueue(7)
+q.enqueue(9)
+q.enqueue(9)
+q.enqueue(9)
+q.enqueue(9)
+q.enqueue(9)
+
+print(q.dequeue())
+
+        
