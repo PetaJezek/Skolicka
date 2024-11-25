fddfd72d60b0fb6ed1c8f378d200e2c047b3e3ad
Petr Ježek <petr.jezek1@gmail.com>
2024-11-25 23:42:38 +0100

    ¨druhy den na strahove

--- Algoritmizace/tree_set.py
+++ Algoritmizace/tree_set.py
@@ -1,3 +1,50 @@
+class Node():
+    def __init__(self, value = None, right = None, left = None):
+        self.right = right
+        self.left = left
+        self.value = value
+    
 class TreeSet():
-    def __init__(self, root = None):
-        self.root = root
+    def __init__(self, root: Node):
+        self.root = None
+    def contains(self, x: int):
+        contains = False
+        current_node = self.root
+        while contains == False:
+            if current_node.value == None:
+                break
+            elif x == current_node.value:
+                contains = True
+            elif x > current_node.value:
+                current_node = current_node.right
+            elif x < current_node.value:
+                current_node = current_node.left
+        return contains
+    def add(self, x: int):
+        if self.contains(x):
+            return
+        else:
+            current_node = self.root
+            while True:
+                if current_node.value == None:
+                    current_node.value = x
+                    break
+                elif x > current_node.value:
+                    current_node = current_node.right
+                elif x < current_node.value:
+                    current_node = current_node.left
+        return
+    def remove(self, x: int):
+        if self.contains(x):
+            current_node = self.root
+            parent = None
+
+            while True:
+                if x == current_node.value:
+                    while
+
+        else:
+            return
+   
+    
+            
